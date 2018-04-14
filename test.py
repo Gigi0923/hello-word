d748798af62fca174c87192263cf5e84b609c514
Gigi0923 <36295851+Gigi0923@users.noreply.github.com>
2018-04-14 10:22:26 +0800

    import file
    import file1

---
+++ test.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium webdriver.supoort import expected_conditons as EC
+import time
+from time import sleep
+import sys
+sys.path.append('./')
+sys.path.appen('..')
+import unittest
+from HTMLTestRunner import HTMLTestRunner
+import os
+import os.path
+import smtplib
+from email.header import header
+from email.mime.text import MIMETEXT
+from email.mimie.Multipart import MIMEMultipart
+import logging
+
+
